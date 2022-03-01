80bd18eab90c9307036c24f7b7443789aefafe14
kapilgoel25 <kapilgoel533@gmail.com>
2022-03-01 22:38:07 +0530

    sidebar first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom'; 
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import Team from './pages/Team';
+import Messages from './pages/Messages'
+import Support from './pages/Support'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+            <Route path="/" exact element={<Home/>}/>
+            <Route path="/reports" element={<Reports/>}/>
+            <Route path="/products" element={<Products/>}/>
+            <Route path="/team" element={<Team/>}/>
+            <Route path="/messages" element={<Messages/>}/>
+            <Route path="/support" element={<Support/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
